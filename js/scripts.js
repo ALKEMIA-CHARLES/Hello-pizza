1570450fae7c0a1f246e00f23c928b628ab06d56
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-10-03 14:53:58 +0300

    added js and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 var totalOrderprice = [];
 
+var size = ["small", "medium", "large"];
+var crust = ["crispy", "stuffed", "Gluten-free"];
+var toppings = ["chicken", "hawaiian", "borewores", "pepperoni", "vegeterian"];
+
+var size = $("#sizeSelect").val();
+var crust = $("#crustSelect").val();
+var toppings = $("Glutenfree").val();
+
 var pizza = {
   size: ["small", "medium", "large"],
   crust: {
@@ -21,63 +29,82 @@ var pizza = {
   }
 };
 
-function placeorder(size, crust, toppings, price) {
+function Pizza(size, crust, toppings) {
   this.size = size;
   this.crust = crust;
   this.toppings = toppings;
   this.price = 0;
 }
 
-placeorder.prototype.costOfPizza = function() {
-  if (this.size === pizza.size[0]) {
+// var placeorder = new Pizza(size, crust, toppings);
+
+Pizza.prototype.costOfPizza = function() {
+  if (this.size === small) {
     this.price += 100;
-  } else if (this.size === pizza.size[1]) {
+  } else if (this.size === medium) {
     this.price += 200;
-  } else if (this.size === pizza.size[2]) {
+  } else if (this.size === large) {
     this.price += 300;
   }
-  if (this.crust === pizza.crust[pizza.crust.crispy]) {
+  if (this.crust === crispy) {
     this.price += 50;
-  } else if (this.crust === pizza.crust[pizza.crust.stuffed]) {
+  } else if (this.crust === stuffed) {
     this.price += 100;
-  } else if (this.crust === pizza.crust[pizza.crust.glutenFree]) {
+  } else if (this.crust === glutenFree) {
     this.price += 200;
   }
-  if (this.toppings === pizza.toppings[pizza.toppings.Chicken]) {
+  if (this.toppings === Chicken) {
     this.price += 50;
-  } else if (this.toppings === pizza.toppings[pizza.toppings.Hawaiian]) {
+  } else if (this.toppings === Hawaiian) {
     this.price += 50;
-  } else if (this.toppings === pizza.toppings[pizza.toppings.Borewores]) {
+  } else if (this.toppings === Borewores) {
     this.price += 50;
-  } else if (this.toppings === pizza.toppings.Pepperoni) {
+  } else if (this.toppings === Pepperoni) {
     this.price += 50;
-  } else if (this.toppings === pizza.toppings.Vegeterian) {
+  } else if (this.toppings === Vegeterian) {
     this.price += 50;
   }
   return this.price;
 };
 
-placeorder.prototype.deliveryCost = function() {
-  return this.deliveryFee;
-};
-placeorder.prototype.totalCost = function() {
-  var shoppingCartTotal = 0;
-  for (var order = 0; order < totalOrderprice.length; order++) {
-    shoppingCartTotal += totalOrderprice[order];
-  }
-  return shoppingCartTotal;
-};
+// placeorder.prototype.deliveryCost = function() {
+//   return this.deliveryFee;
+// };
+// placeorder.prototype.totalCost = function() {
+//   var shoppingCartTotal = 0;
+//   for (var order = 0; order < totalOrderprice.length; order++) {
+//     shoppingCartTotal += totalOrderprice[order];
+//   }
+//   return shoppingCartTotal;
+// };
 
-function shippingAddress(cityName, cityStreet, nameOfBuilding, phoneNumber) {
-  this.cityName = cityName;
-  this.cityStreet = cityStreet;
-  this.nameOfBuilding = nameOfBuilding;
-  this.phoneNumber = phoneNumber;
-  this.deliveryAddress =
-    cityStreet +
-    "" +
-    nameOfBuilding +
-    "" +
-    "we will call you on this phone number" +
-    phoneNumber;
-}
+// function ShippingAddress(
+//   cityName,
+//   cityStreet,
+//   nameOfBuilding,
+//   phoneNumber,
+//   deliveryAddress
+// ) {
+//   this.cityName = cityName;
+//   this.cityStreet = cityStreet;
+//   this.nameOfBuilding = nameOfBuilding;
+//   this.phoneNumber = phoneNumber;
+//   this.deliveryAddress =
+//     cityStreet +
+//     "" +
+//     nameOfBuilding +
+//     "" +
+//     "we will call you on this phone number" +
+//     phoneNumber;
+// }
+
+$(document).ready(function() {
+  var size = $("#sizeSelect").val();
+  var crust = $("crustSelect").val();
+  var toppings = $("#Glutenfree").val();
+
+  var placeorder = new Pizza(size, crust, toppings);
+  placeorder.price;
+
+  $("h3.card-text").text("Your total price is " + placeorder.price);
+});
